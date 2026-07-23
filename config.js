4ba35bf0a6aca07d78f03455fba34093cb36ca99
staporn123 <Sataporn282537@gmail.com>
2026-07-23 17:03:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = Object.freeze({
   APP_NAME: 'PEA School Water Safety 2569',
-  API_URL: 'https://script.google.com/macros/s/AKfycbx5q9Kjtk7-ZJEwgpVZCJTYGqcCXn3sAbUoPadRXO3hT23s8lRa7C544c3sNuLyBd3xdA/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbzCa3_lpE3UyGdTUgfxv1mZb22RKyouRORigDKpqhB78d9fekai9LS0uZIansIWLvBjPw/exec',
   API_TIMEOUT_MS: 90000,
   MAP_DEFAULT_CENTER: [17.65, 103.95],
   MAP_DEFAULT_ZOOM: 9,
